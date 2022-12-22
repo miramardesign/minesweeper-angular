abebc21ac0d4be99654f04f794edacd3a8cc67d7
mhazzard <miramardesign@gmail.com>
2022-12-22 19:04:48 -0300

    ionic rem from angular.json

--- src/app/core/game.reducer.ts
+++ src/app/core/game.reducer.ts
@@ -1,22 +1,25 @@
-import { CellData, GameStateDisplay, GameTypesKeys } from "src/assets/types/mineTypes";
+import {
+  CellData,
+  GameStateDisplay,
+  GameTypes,
+  GameTypesKeys,
+} from 'src/assets/types/mineTypes';
+import { GameActions, GameActionType } from 'src/assets/types/state';
 
+export const gridSizeSeparate: keyof GameTypes = 'test';
 
-export const gridSizeSeparate: keyof GameTypes = "test";
-
-export const emptyMineDataStructure = getGridDataStructureFromGameConfig(getGameSize(gridSizeSeparate));
+// export const emptyMineDataStructure = getGridDataStructureFromGameConfig(getGameSize(gridSizeSeparate));
 
 export type GameState = {
-    isLost: boolean;
-    isGameOver: boolean;
-    isGameStarted: boolean;
-    uncoveredCells: number;
-    flagsPlaced: number
-    gridSize: GameTypesKeys;
-    gameStateDisplay: GameStateDisplay;
-    mineData: CellData[][];
-
+  isLost: boolean;
+  isGameOver: boolean;
+  isGameStarted: boolean;
+  uncoveredCells: number;
+  flagsPlaced: number;
+  gridSize: GameTypesKeys;
+  gameStateDisplay: GameStateDisplay;
+  mineData: CellData[][]; //emptyMineDataStructure
 };
-
 
 export const initialState: GameState = {
   isLost: false,
@@ -25,15 +28,86 @@ export const initialState: GameState = {
   uncoveredCells: 0,
   flagsPlaced: 0,
   gameStateDisplay: GameStateDisplay.UNSTARTED,
-  gridSize: gridSizeSeparate,
- // gridSize: "beginner", 
- 
- mineData: emptyMineDataStructure,
-//  mineDataOlde:  JSON.parse(JSON.stringify(emptyMineDataStructure)),
+  //ridSize: gridSizeSeparate,
+  gridSize: 'beginner',
+
+  //mineData: emptyMineDataStructure,
+  mineData: [],
+  //  mineDataOlde:  JSON.parse(JSON.stringify(emptyMineDataStructure)),
 };
 
+/**
+ * reset the game state...
+ * @param initialState
+ * @returns
+ */
+const initReducer = (initialState: GameState) => {
+  return initialState;
+};
 
+export function reducer(state = initialState, action: GameActions): GameState {
+  switch (action.type) {
+    case GameActionType.TOGGLE_LOST:
+      return {
+        ...state,
+        isLost: !state.isLost,
+        gameStateDisplay: GameStateDisplay.LOSE,
+      };
+    case GameActionType.SET_START:
+      return {
+        ...state,
+        isGameStarted: action.payload,
+      };
+    case GameActionType.SET_END:
+      return {
+        ...state,
+        isGameOver: action.payload,
+      };
+    case GameActionType.UPDATE_UNCOVER_CELL:
+      return {
+        ...state,
+        uncoveredCells: action.payload,
+      };
+    case GameActionType.CHANGE_GAMESTATE_DISPLAY:
+      return {
+        ...state,
+        gameStateDisplay: action.payload,
+      };
+    case GameActionType.INCREMENT_UNCOVER_CELL:
+      return {
+        ...state,
+        uncoveredCells: state.uncoveredCells + 1,
+      };
 
-
-
-export function reducer(state = initialState, action): GameState { }
+    case GameActionType.INCREMENT_FLAGS_PLACED:
+      return {
+        ...state,
+        flagsPlaced: state.flagsPlaced + 1,
+      };
+    case GameActionType.DECREMENT_FLAGS_PLACED:
+      return {
+        ...state,
+        flagsPlaced: state.flagsPlaced - 1,
+      };
+    case GameActionType.CHOOSE_SIZE:
+      return {
+        ...state,
+        gridSize: action.payload,
+      };
+    case GameActionType.SET_MINE_DATA:
+      return {
+        ...state,
+        mineData: action.payload,
+      };
+    // case GameActionType.SET_MINE_DATA_OLDE:
+    //   return {
+    //     ...state,
+    //     mineDataOlde: action.payload,
+    //   };
+    case GameActionType.RESET_GAME:
+      return initReducer(action.payload);
+    default:
+      console.error('Action not implemented', action);
+      throw new Error();
+  }
+}
